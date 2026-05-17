fc1045144d49cc3f93e0f13e291ec4c1c27c06a0
Andres <atm1996.ast@gmail.com>
2026-05-16 20:28:08 -0600

    cambios para test ci

--- src/test/java/com/easybroker/project/EasybrokerApplicationTests.java
+++ src/test/java/com/easybroker/project/EasybrokerApplicationTests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(classes = EasybrokerApplication.class)
 @ActiveProfiles("test")
 class EasybrokerApplicationTests {
 
